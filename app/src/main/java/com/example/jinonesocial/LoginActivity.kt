c62ad2c62f559c1f57bc265bab0d523cb996271a
febru <februarycl@naver.com>
2023-02-05 17:23:31 +0900

    회원가입 + 로그인

--- app/src/main/java/com/example/jinonesocial/LoginActivity.kt
+++ app/src/main/java/com/example/jinonesocial/LoginActivity.kt
@@ -1,26 +1,94 @@
 package com.example.jinonesocial
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.example.jinonesocial.databinding.ActivityLoginBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
 
-    private var auth : FirebaseAuth? = null
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding: ActivityLoginBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
-
         auth = Firebase.auth
 
-        val member_btn = findViewById<Button>(R.id.member_btn)
-        member_btn.setOnClickListener{
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        binding.memberBtn.setOnClickListener {
+
+            var idText = binding.idEdit.text
+            var passwordText = binding.passwordEdit.text
+
+            createAccount(idText.toString(), passwordText.toString())
         }
 
+        binding.loginBtn.setOnClickListener {
+            var idText = binding.idEdit.text
+            var passwordText = binding.passwordEdit.text
+
+            signIn(idText.toString(), passwordText.toString())
+        }
+
+
     }
+    private fun createAccount(email: String, password: String) {
+
+        if (email.isNotEmpty() && password.isNotEmpty()) {
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(
+                            this, "계정 생성 완료.",
+                            Toast.LENGTH_SHORT
+                        ).show()
+
+                    } else {
+                        Toast.makeText(
+                            this, "계정 생성 실패",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+        }
+    }
+    private fun signIn(email: String, password: String) {
+        if (email.isNotEmpty() && password.isNotEmpty()) {
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(
+                            baseContext, "로그인에 성공 하였습니다.",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        moveMainPage(auth.currentUser)
+                    } else {
+                        Toast.makeText(
+                            baseContext, "로그인에 실패 하였습니다.",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+        }
+    }
+
+    private fun moveMainPage(user: FirebaseUser?) {
+        if (user != null){
+            startActivity(Intent(this,MainActivity::class.java))
+            finish()
+        }
+    }
+
+
 }
